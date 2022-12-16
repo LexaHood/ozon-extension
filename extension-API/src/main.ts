7e53c26282a0d80c8ab654570b0cfd00625a3eff
LexaHood <allpetr@list.ru>
2022-12-16 17:54:03 +0300

    Добавил пустое API

---
+++ extension-API/src/main.ts
@@ -0,0 +1,18 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('API_PORT');
+
+  Logger.log(`Api started at port: ${port}`, `main.ts`);
+
+  await app.listen(port);
+}
+// Задержка для того, чтобы успел подцепиться дебаггер при аттаче. Иначе продебажить bootstrap не выходит
+setTimeout(() => bootstrap(), 1500);
