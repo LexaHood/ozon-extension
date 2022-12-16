191cb30fdfcb1a1d2b2bd9152be0d28e9d7725e8
LexaHood <allpetr@list.ru>
2022-12-16 23:20:23 +0300

    Добавил конфиг

--- extension-API/src/main.ts
+++ extension-API/src/main.ts
@@ -3,14 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   const port = configService.get('API_PORT');
 
-  Logger.log(`Api started at port: ${port}`, `main.ts`);
+  Logger.log(`API started at port: ${port}`, `main.ts`);
 
   await app.listen(port);
 }
